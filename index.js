a9967937a5f87133846ede3abe13c25e9164a0df
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-10-30 20:24:10 +0300

    Transferred from main account

--- index.js
+++ index.js
@@ -16,3 +16,20 @@ function returnsAnAnonymousFunction() {
     };
 }
 
+// Example usage:
+receivesAFunction(() => console.log("Callback executed!"));
+
+const namedFunc = returnsANamedFunction();
+namedFunc();
+
+const anonymousFunc = returnsAnAnonymousFunction();
+anonymousFunc();
+
+// Explanation:
+
+//     receivesAFunction: This function takes a callback function as an argument and calls it. You can pass any function to it.
+
+//     returnsANamedFunction: This function returns a named function. You can name it namedFunction, but you can also change the name inside the function definition.
+
+//     returnsAnAnonymousFunction: This function returns an anonymous function that doesn’t have a name.
+
